b0c68abb707e634a04844f53bd3bf279c59e0d50
Ruben <halmanrf@gmail.com>
2021-07-06 00:10:33 +0200

    replace json by enum

--- src/main/rules/DMLStatementInLoop.ts
+++ src/main/rules/DMLStatementInLoop.ts
@@ -1,14 +1,15 @@
-import * as rules from '../config/rules.json';
 import {IRuleDefinition} from '../libs/IRuleDefinition';
 import {Flow} from '../models/Flow';
 import {FlowElement} from '../models/FlowElement';
 import {RuleResult} from '../models/RuleResult';
+import {RuleDefinitions} from '../ruledefinitions/RuleDefinitions';
+import { RuleInfo } from '../ruledefinitions/RuleInfo';
 
 export class DMLStatementInLoop implements IRuleDefinition{
 
   constructor() {
-    const rule = rules.rules.find(rule => rule.name === 'DMLStatementInLoop');
-    this.name = rule.name;
+    const rule = RuleInfo(RuleDefinitions.DMLStatementInLoop);
+    this.name = RuleDefinitions.DMLStatementInLoop;
     this.label = rule.label;
     this.text = rule.text;
   }
