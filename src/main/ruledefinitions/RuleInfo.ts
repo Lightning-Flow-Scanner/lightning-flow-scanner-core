3e5a0ffe3c2f3df1f902b42c781de41c9227c010
Ruben <halmanrf@gmail.com>
2021-09-26 00:18:30 +0200

    update rule info

--- src/main/ruledefinitions/RuleInfo.ts
+++ src/main/ruledefinitions/RuleInfo.ts
@@ -16,12 +16,12 @@ export function RuleInfo(ruleDefinitions : RuleDefinitions) {
     case RuleDefinitions.HardcodedIds:
       return {
         label: 'Hardcoded Ids',
-        text: 'IDs are org-specific, so don’t hard-code new or existing IDs. Instead, pass them into variables when the flow starts. You can do so, for example, by using merge fields in URL parameters or by using a Get Records element.'
+        text: 'IDs are org-specific, so don’t hard-code IDs. Instead, pass them into variables when the flow starts. You can do so, for example, by using merge fields in URL parameters or by using a Get Records element.'
       };
     case RuleDefinitions.MissingFlowDescription:
       return {
         label: 'Missing flow description',
-        text: 'Flow descriptions are the closest thing to documentation. It is recommended to provide information about where it is used and what it will do.'
+        text: 'Descriptions are useful for documentation purposes. It is recommended to provide information about where it is used and what it will do.'
       };
     case RuleDefinitions.MissingFaultPath:
       return {
@@ -31,17 +31,17 @@ export function RuleInfo(ruleDefinitions : RuleDefinitions) {
     case RuleDefinitions.MissingNullHandler:
       return {
         label: 'Missing null handlers',
-        text: 'If a Get Records operation does not find any data it will return null. You should use a decision element on that variable to check if the result is not null.'
+        text: 'If a Get Records operation does not find any data it will return null. Use a decision element on the operation result variable to validate that the result is not null.'
       };
     case RuleDefinitions.UnconnectedElements:
       return {
         label: 'Unconnected elements',
-        text: 'Removing unconnected elements which are not being used by the Flow makes your Flow more performant and maintainable.'
+        text: 'Removing unconnected elements which are not being used by the Flow makes your Flow more efficient and maintainable.'
       };
     case RuleDefinitions.UnusedVariables:
       return {
         label: 'Unused variables',
-        text: 'Removing unconnected variables which are not being used by the Flow makes your Flow more performant and maintainable.'
+        text: 'Removing unconnected variables which are not being used by the Flow makes your Flow more efficient and maintainable.'
       };
   }
 }
