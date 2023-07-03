5d76917570e5fb031fce312506773380162a7a14
Ruben <halmanrf@gmail.com>
2023-07-03 23:08:02 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { json } from 'stream/consumers';
 import { IRuleDefinition } from './main/interfaces/IRuleDefinition';
 import { IRulesConfig } from './main/interfaces/IRulesConfig';
-import { ApplyOverrides } from './main/libs/ApplyOverrides';
 import { FixFlows } from './main/libs/FixFlows';
 import { GetRuleDefinitions } from './main/libs/GetRuleDefinitions';
 import { ScanFlows } from './main/libs/ScanFlows';
@@ -10,59 +8,46 @@ import { ScanResult } from './main/models/ScanResult';
 
 export function getRules(ruleNames?: string[]): IRuleDefinition[] {
   if (ruleNames) {
-    const map = new Map<string, string>();
-    for (const str of ruleNames) {
-      map.set(str, 'error');
-    }
-    return GetRuleDefinitions(map);
+    const ruleSeverityMap = new Map<string, string>(ruleNames.map((name) => [name, 'error']));
+    return GetRuleDefinitions(ruleSeverityMap);
   } else {
     return GetRuleDefinitions();
   }
 }
 
 export function scan(flows: Flow[], ruleOptions?: IRulesConfig): ScanResult[] {
-  let scanResults: ScanResult[];
-  const ruleNameSeverityMap: Map<string, string> = new Map();
+  const ruleNameSeverityMap = new Map<string, string>();
 
-  if (ruleOptions && ruleOptions.rules) {
-    Object.entries(ruleOptions.rules).forEach(([ruleName, rule]) => {
+  if (ruleOptions?.rules) {
+    for (const [ruleName, rule] of Object.entries(ruleOptions.rules)) {
       const ruleSeverity = rule.severity;
       ruleNameSeverityMap.set(ruleName, ruleSeverity);
-    });
-
-    if (ruleNameSeverityMap.size > 0) {
-      scanResults = ScanFlows(flows, ruleNameSeverityMap);
-    } else {
-      scanResults = ScanFlows(flows);
     }
-  } else {
-    scanResults = ScanFlows(flows);
   }
 
-if (ruleOptions && ruleOptions.exceptions) {
-  Object.entries(ruleOptions.exceptions).forEach(([exceptionName, exceptionElements]) => {
-    scanResults.forEach((scanResult) => {
-      if (scanResult.flow.label[0] === exceptionName) {
-        scanResult.ruleResults.forEach((ruleResult) => {
-          if (exceptionElements[ruleResult.ruleName]) {
-            const detailNames = exceptionElements[ruleResult.ruleName];
-            const filteredDetails = ruleResult.details.filter((detail) => {
-              return !detailNames.includes(detail.name);
-            });
-            ruleResult.details = filteredDetails;
-            ruleResult.occurs = filteredDetails.length > 0;
-          }
-        });
-      }
-    });
-  });
-}
+  let scanResults: ScanResult[] = ScanFlows(flows, ruleNameSeverityMap);
 
-  
+  if (ruleOptions?.exceptions) {
+    for (const [exceptionName, exceptionElements] of Object.entries(ruleOptions.exceptions)) {
+      for (const scanResult of scanResults) {
+        if (scanResult.flow.label[0] === exceptionName) {
+          for (const ruleResult of scanResult.ruleResults) {
+            if (exceptionElements[ruleResult.ruleName]) {
+              const detailNames = exceptionElements[ruleResult.ruleName];
+              const filteredDetails = ruleResult.details.filter((detail) => {
+                return !detailNames.includes(detail.name);
+              });
+              ruleResult.details = filteredDetails;
+              ruleResult.occurs = filteredDetails.length > 0;
+            }
+          }
+        }
+      }
+    }
+  }
 
   return scanResults;
 }
-
 
 export function fix(flows: Flow[]): ScanResult[] {
   return FixFlows(flows);
