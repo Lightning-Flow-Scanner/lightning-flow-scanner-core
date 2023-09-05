07ec31cd72607bea564efbb1ac027bf5baf7c6bb
Ruben <halmanrf@gmail.com>
2023-09-05 16:57:21 +0200

    Update FlowType.ts

--- src/main/models/FlowType.ts
+++ src/main/models/FlowType.ts
@@ -2,8 +2,8 @@ export class FlowType {
 
   public static backEndTypes = ['AutoLaunchedFlow', 'CustomEvent', 'InvocableProcess', 'Orchestrator', 'EvaluationFlow', 'ActionCadenceAutolaunchedFlow'];
   public static processBuilder = ['Workflow'];
-  public static visualTypes = ['Flow', 'LoginFlow', 'RoutingFlow', 'Appointments', 'ActionCadenceStepFlow', 'ContactRequestFlow', 'ContactRequestFlow', 'CustomerLifecycle', 'FieldServiceMobile', 'FieldServiceWeb', 'Survey', 'SurveyEnrich'];
-  public static unsupportedTypes = ['IndividualObjectLinkingFlow', 'CheckoutFlow', 'FSCLending', 'FSCLending', 'LoyaltyManagementFlow'];
+  public static visualTypes = ['Flow', 'IndividualObjectLinkingFlow', 'LoginFlow', 'RoutingFlow', 'Appointments', 'ActionCadenceStepFlow', 'ContactRequestFlow', 'ContactRequestFlow', 'CustomerLifecycle', 'FieldServiceMobile', 'FieldServiceWeb', 'Survey', 'SurveyEnrich'];
+  public static unsupportedTypes = ['CheckoutFlow', 'FSCLending', 'FSCLending', 'LoyaltyManagementFlow'];
 
   public static allTypes = function () {
     return [...this.backEndTypes, ...this.processBuilder, ...this.visualTypes];
