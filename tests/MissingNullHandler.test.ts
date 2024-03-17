69c22c102f36faba678cdbc0e8227a3758b95cfa
Ruben <halmanrf@gmail.com>
2024-03-17 19:30:05 +0100

    add test case

--- tests/MissingNullHandler.test.ts
+++ tests/MissingNullHandler.test.ts
@@ -1,21 +1,13 @@
 import { assert, expect } from 'chai';
 import 'mocha';
 import * as core from '../src'
-
 import CreateANewAccountWithChild from './testfiles/MissingNullHandler.json';
+import Subflow_MissingNullHandler from './testfiles/Subflow_MissingNullHandler.json';
 
-describe('When scanning a flow without null handlers', () => {
+describe('The MissingNullHandler Rule', () => {
     let flow: core.Flow;
 
-    before('arrange', () => {
-        // ARRANGE
-        flow = new core.Flow({
-            path: './testfiles/tc.flow-meta.xml',
-            xmldata: CreateANewAccountWithChild,
-        });
-    });
-
-    it('missing null handler should have a result', () => {
+    it('Should have a result when missing', () => {
         const ruleConfig = {
             rules:
             {
@@ -25,8 +17,32 @@ describe('When scanning a flow without null handlers', () => {
                 },
             }
         };
+        flow = new core.Flow({
+            path: './testfiles/tc.flow-meta.xml',
+            xmldata: CreateANewAccountWithChild,
+        });
         const results: core.ScanResult[] = core.scan([flow], ruleConfig);
         expect(results[0].ruleResults[0].ruleName).to.equal('MissingNullHandler')
         expect(results[0].ruleResults[0].occurs).to.equal(true);
+    });
+
+    it('Should have no result for subflow output variables ', () => {
+        const ruleConfig = {
+            rules:
+            {
+                MissingNullHandler:
+                {
+                    severity: 'error',
+                },
+            }
+        };
+        flow = new core.Flow({
+            path: './testfiles/Subflow_MissingNullHandler.flow-meta.xml',
+            xmldata: Subflow_MissingNullHandler,
+        });
+        const results: core.ScanResult[] = core.scan([flow], ruleConfig);
+        expect(results[0].ruleResults[0].ruleName).to.equal('MissingNullHandler')
+        expect(results[0].ruleResults[0].occurs).to.equal(true);
+        expect(results[0].ruleResults[0].details.length).to.equal(1);
     });
 });
