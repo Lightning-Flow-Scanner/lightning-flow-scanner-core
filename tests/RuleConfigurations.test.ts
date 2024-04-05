ac45c79b3dfdc06a73dea5d4cce846812b72dd7c
Ruben <halmanrf@gmail.com>
2024-04-05 03:21:37 +0200

    rf test

--- tests/RuleConfigurations.test.ts
+++ tests/RuleConfigurations.test.ts
@@ -2,7 +2,7 @@ import { assert, expect } from 'chai';
 import 'mocha';
 import * as core from '../src'
 import unconnectedElement from './testfiles/UnconnectedElement.json';
-import waitForOnly from './testfiles/WaitForOnly.json';
+import Hidenav from './testfiles/hidenav.json';
 
 describe('Rule Configurations ', () => {
 
@@ -84,4 +84,77 @@ describe('Rule Configurations ', () => {
     const results: core.ScanResult[] = core.scan([flow2], ruleConfig);
     expect(results[0].ruleResults.length).to.equal(1);
   });
+
+  it('Multiple Expressions are individually checked', () => {
+    flow2 = new core.Flow({
+      path: './testfiles/Async_OnlyTransaction.flow-meta.xml',
+      xmldata: unconnectedElement,
+    });
+    const ruleConfig = {
+        rules: {
+          APIVersion: {
+            severity: "error",
+            expression: ">50",
+          },
+          CopyAPIName: {
+            severity: "error",
+          },
+          DMLStatementInLoop: {
+            severity: "error",
+          },
+          DuplicateDMLOperation: {
+            severity: "error",
+          },
+          FlowDescription: {
+            severity: "error",
+          },
+          FlowName: {
+            severity: "error",
+            expression: "[A-Za-z0-9]+_[A-Za-z0-9]+",
+          },
+          HardcodedId: {
+            severity: "error",
+          },
+          MissingFaultPath: {
+            severity: "error",
+          },
+          MissingNullHandler: {
+            severity: "error",
+          },
+          SOQLQueryInLoop: {
+            severity: "error",
+          },
+          UnconnectedElement: {
+            severity: "error",
+          },
+          UnusedVariable: {
+            severity: "error",
+          },
+        },
+      };
+    const results: core.ScanResult[] = core.scan([flow2], ruleConfig);
+    expect(results[0].ruleResults.find((r) => r.ruleName === 'FlowName')?.occurs).to.equal(false);
+  });
+
+    it(' should give result', () => {
+      flow2 = new core.Flow({
+            path: './testfiles/NewAccountName.flow-meta.xml',
+            xmldata: Hidenav,
+        });
+        const ruleConfig = {
+            rules:
+            {
+                CustomNamingConvention:
+                {
+                    severity: 'error',
+                    path: './src/data/CustomRuleExample.ts'
+                }
+            }
+        };
+
+        const results: core.ScanResult[] = core.scan([flow2], ruleConfig);
+        expect(results[0].ruleResults[0].occurs).to.equal(true);
+    });
+
+
 });
