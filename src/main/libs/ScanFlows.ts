57ec94dcd7699a5275411f6603016e73ce786744
Ruben <halmanrf@gmail.com>
2024-05-06 15:55:20 +0200

    add errormessage on rule failure

--- src/main/libs/ScanFlows.ts
+++ src/main/libs/ScanFlows.ts
@@ -1,8 +1,10 @@
-import { GetRuleDefinitions } from './GetRuleDefinitions';
-import * as core from '../../main/internals/internals';
+import { GetRuleDefinitions } from "./GetRuleDefinitions";
+import * as core from "../../main/internals/internals";
 
-export function ScanFlows(flows: core.Flow[], ruleOptions?: core.IRulesConfig): core.ScanResult[] {
-
+export function ScanFlows(
+  flows: core.Flow[],
+  ruleOptions?: core.IRulesConfig
+): core.ScanResult[] {
   const flowResults: core.ScanResult[] = [];
 
   let selectedRules: core.IRuleDefinition[] = [];
@@ -17,34 +19,39 @@ export function ScanFlows(flows: core.Flow[], ruleOptions?: core.IRulesConfig): 
   }
 
   for (const flow of flows) {
-
-    try {
-      const ruleResults: core.RuleResult[] = [];
-      for (const rule of selectedRules) {
+    const ruleResults: core.RuleResult[] = [];
+    for (const rule of selectedRules) {
+      try {
         if (rule.supportedTypes.includes(flow.type[0])) {
-          try {
-            let config = undefined;
-
-            if (ruleOptions && ruleOptions['rules'] && ruleOptions['rules'][rule.name]) {
-              config = ruleOptions['rules'][rule.name];
-            }
-            const result = (config && Object.keys(config).length > 0) ? rule.execute(flow, config) : rule.execute(flow);
-            if (result.severity !== rule.severity) {
-              result.severity = rule.severity;
-            }
-            ruleResults.push(result);
-          } catch (error) {
-            throw new error("Something went wrong while executing " + rule.name + " in the Flow: '" + flow.name + "'");
+          let config = undefined;
+          if (
+            ruleOptions &&
+            ruleOptions["rules"] &&
+            ruleOptions["rules"][rule.name]
+          ) {
+            config = ruleOptions["rules"][rule.name];
           }
+          const result =
+            config && Object.keys(config).length > 0
+              ? rule.execute(flow, config)
+              : rule.execute(flow);
+          if (result.severity !== rule.severity) {
+            result.severity = rule.severity;
+          }
+          ruleResults.push(result);
         } else {
           ruleResults.push(new core.RuleResult(rule, []));
         }
+      } catch (error) {
+        let message =
+          "Something went wrong while executing " +
+          rule.name +
+          " in the Flow: '" +
+          flow.name;
+        ruleResults.push(new core.RuleResult(rule, [], message));
       }
-      flowResults.push(new core.ScanResult(flow, ruleResults));
     }
-    catch (error) {
-      console.log(error.message)
-    }
+    flowResults.push(new core.ScanResult(flow, ruleResults));
   }
 
   return flowResults;
