8c97d9b645549d229940d4e51007353b8b1f278f
Kyle Capehart (Huntersville) <kyleacapehart@gmail.com>
2024-05-07 16:35:37 -0400

    update rule description and readme

--- src/main/rules/InactiveFlow.ts
+++ src/main/rules/InactiveFlow.ts
@@ -1,4 +1,3 @@
-import * as IdPrefixes from '../data/IdPrefixes.json';
 import { RuleCommon } from '../models/RuleCommon';
 import * as core from '../internals/internals';
 
@@ -8,7 +7,7 @@ export class InactiveFlow extends RuleCommon implements core.IRuleDefinition {
     super({
       name: 'InactiveFlow',
       label: 'Inactive Flow',
-      description: 'Avoid having inactive flows in your org.',
+      description: 'It\'s better to delete flows that are no longer used rather than have them be inactive. Inactive flows can still delete records when testing them, and parent flows will run an inactive subflow if no active version is found.',
       supportedTypes: core.FlowType.allTypes(),
       docRefs: [],
       isConfigurable: true, 
@@ -19,7 +18,6 @@ export class InactiveFlow extends RuleCommon implements core.IRuleDefinition {
 
   public execute(flow: core.Flow): core.RuleResult {
     const inactiveFlows = [];
-    console.log(flow.name);
     for (const node of flow.elements) {
         const nodeElementString = JSON.stringify(node.element);
         if (node.subtype == "status" && nodeElementString != '\"Active\"') {
