04b4846f293557da015d330a399f6b5e9a3d120c
junners <jose.junryan@gmail.com>
2024-05-26 01:25:01 -0700

    chore: rollup configurations

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,20 @@
+import typescript from '@rollup/plugin-typescript';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default {
-    input: "out/index.js",
+    input: "src/index.ts",
     output: {
-        file: 'dist/LFS.js',
+        dir: "out",
         format: 'umd',
-        name: 'lightningflowscanner'
-    }
+        name: 'lightningflowscanner',
+        sourcemap: true,
+        globals: {
+            "path-browserify": "p",
+            xmlbuilder2: "xmlbuilder2"
+        }
+    },
+    plugins: [nodePolyfills(), typescript(), nodeResolve({preferBuiltins:true}), nodePolyfills()],
+    external: ["path-browserify", "xmlbuilder2"]
 }
