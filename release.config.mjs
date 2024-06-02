f8e36d7b709d961ec2b3e3888ef49616e06509dc
junners <jose.junryan@gmail.com>
2024-06-01 21:23:55 -0700

    build: use semantic-release for commits to master

--- release.config.mjs
+++ release.config.mjs
@@ -2,9 +2,29 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: [{ name: "master", prerelease: true }],
+  branches: [
+    {
+      name: "master",
+    },
+  ],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "fix", release: "patch" },
+          { type: "feat", release: "patch" },
+          { type: "build", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "test", release: "patch" },
+        ],
+      },
+    ],
     [
       "@semantic-release/changelog",
       {
