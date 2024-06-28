3c1e24c8830d145047171b31fb815164c93e8dd4
junners <jose.junryan@gmail.com>
2024-06-28 16:58:36 -0700

    chore: rename release config to semantic release for consistency

---
+++ semantic-release/release.config.mjs
@@ -0,0 +1,44 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    {
+      name: "master",
+      channel: "beta"
+    },
+  ],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { type: "fix", release: "patch" },
+          { type: "feat", release: "patch" },
+          { type: "build", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "test", release: "patch" },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
