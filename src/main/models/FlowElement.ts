f9596892282682fcbf8bf9f78b3d13298cacb66c
junners <jose.junryan@gmail.com>
2024-10-19 23:17:24 -0700

    chore: add tests for exceptions

--- src/main/models/FlowElement.ts
+++ src/main/models/FlowElement.ts
@@ -1,7 +1,11 @@
 export class FlowElement {
   public subtype: string;
   public metaType: string;
-  public element: object = {};
+  public element: string | object = {};
+  public connectors?: object[];
+  public name?: string;
+  public locationX?: string;
+  public locationY?: string;
 
   constructor(metaType: string, subtype: string, element: object) {
     this.element = element;
