865489f93a03233d810dae2449664c4d75b29ed1
junners <jose.junryan@gmail.com>
2024-11-16 00:59:41 -0800

    feat(RuleEngine): make rule imports for non default rule store rules

--- src/main/libs/DynamicRule.ts
+++ src/main/libs/DynamicRule.ts
@@ -1,8 +1,16 @@
 import { DefaultRuleStore } from "../store/DefaultRuleStore";
-
+import { RuleLoader } from "./RuleLoader";
+import * as p from "path-browserify";
 export class DynamicRule {
   constructor(className: string) {
     if (DefaultRuleStore[className] === undefined || DefaultRuleStore[className] === null) {
+      const customRule = RuleLoader.loadCustomRule(
+        className,
+        p.join(__dirname, `../rules/${className}`)
+      );
+      if (customRule) {
+        return customRule;
+      }
       throw new Error(`Rule \'${className}\' does not exist in the store.`);
     }
     return new DefaultRuleStore[className]();
