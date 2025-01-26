fc146af164c3361af26e6b6fca11e8364dd9a548
junners <jose.junryan@gmail.com>
2025-01-25 23:14:02 -0800

    test: add debug logs for debugging

--- src/main/libs/ParseFlows.ts
+++ src/main/libs/ParseFlows.ts
@@ -8,13 +8,15 @@ export async function ParseFlows(selectedUris: string[]): Promise<ParsedFlow[]> 
   const parseResults: ParsedFlow[] = [];
   for (const uri of selectedUris) {
     try {
+      console.log(`normalize uri ${uri}`);
       const normalizedURI = p.normalize(uri);
       const content = await fs.readFileSync(normalizedURI);
       const xmlString = content.toString();
       const flowObj = convert(xmlString, { format: "object" });
+      console.log(`flowObj ${JSON.stringify(flowObj, null, 2)}`);
       parseResults.push(new ParsedFlow(uri, new Flow(uri, flowObj)));
     } catch (e) {
-      parseResults.push(new ParsedFlow(uri, undefined, e.errorMessage));
+      parseResults.push(new ParsedFlow(uri, undefined, e));
     }
   }
   return parseResults;
