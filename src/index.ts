3a2afa7ebadc3978a79e03ccdb54261ed8e74061
junners <jose.junryan@gmail.com>
2025-02-01 16:23:09 -0800

    fix: exported types to include parsed flow for consumers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-export * from "./main/core";
+import { Flow } from "./main/models/Flow";
+import { FlowAttribute } from "./main/models/FlowAttribute";
+import { FlowElement } from "./main/models/FlowElement";
+import { FlowNode } from "./main/models/FlowNode";
+import { FlowResource } from "./main/models/FlowResource";
+import { ResultDetails } from "./main/models/ResultDetails";
+import { RuleResult } from "./main/models/RuleResult";
+import { ScanResult } from "./main/models/ScanResult";
+import { FlowType } from "./main/models/FlowType";
+import { FlowVariable } from "./main/models/FlowVariable";
+import { ParsedFlow } from "./main/models/ParsedFlow";
+
+import { Compiler } from "./main/libs/Compiler";
+import { fix } from "./main/libs/FixFlows";
+import { getRules } from "./main/libs/GetRuleDefinitions";
+import { parse } from "./main/libs/ParseFlows";
+import { scan } from "./main/libs/ScanFlows";
+
+import type { IRulesConfig } from "./main/interfaces/IRulesConfig";
+import type { IRuleDefinition } from "./main/interfaces/IRuleDefinition";
+
+export {
+  Flow,
+  FlowAttribute,
+  FlowElement,
+  FlowNode,
+  FlowResource,
+  FlowType,
+  FlowVariable,
+  ScanResult,
+  RuleResult,
+  ResultDetails,
+  Compiler,
+  ParsedFlow,
+  getRules,
+  parse,
+  scan,
+  fix,
+};
+export type { IRuleDefinition, IRulesConfig };
