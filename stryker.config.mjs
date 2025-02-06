5ddeb17decb839b9c000cf8e38e316d53868d465
junners <jose.junryan@gmail.com>
2025-02-05 17:38:37 -0800

    test: use stryker for mutation testing quality hardening

---
+++ stryker.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress", "dashboard"],
+  commandRunner: { command: "npm test" },
+  testRunner: "jest",
+  testRunner_comment:
+    "Take a look at https://stryker-mutator.io/docs/stryker-js/jest-runner for information about the jest plugin.",
+  coverageAnalysis: "perTest",
+  mutate: ["src/**/*.ts", "!tests/**/*.test.ts"],
+};
+export default config;
