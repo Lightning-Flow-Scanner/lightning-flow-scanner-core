087c6637f7e7090d8284fa44dd61d56f4dc1a97d
junners <jose.junryan@gmail.com>
2025-03-08 16:18:33 -0800

    docs: optimize rule docs for umd compiled version

--- src/main/rules/CyclomaticComplexity.ts
+++ src/main/rules/CyclomaticComplexity.ts
@@ -7,14 +7,11 @@ export class CyclomaticComplexity extends RuleCommon implements core.IRuleDefini
       {
         name: "CyclomaticComplexity",
         label: "Cyclomatic Complexity",
-        description: `The number of loops and decision rules, plus the number of decisions.
-          Use a combination of 1) subflows and 2) breaking flows into multiple concise trigger ordered flows, 
-          to reduce the cyclomatic complexity within a single flow, ensuring maintainability and simplicity.`,
+        description: `The number of loops and decision rules, plus the number of decisions. Use a combination of 1) subflows and 2) breaking flows into multiple concise trigger ordered flows, to reduce the cyclomatic complexity within a single flow, ensuring maintainability and simplicity.`,
         supportedTypes: core.FlowType.backEndTypes,
         docRefs: [
           {
-            label: `Cyclomatic complexity is a software metric used to indicate the complexity of a program. 
-              It is a quantitative measure of the number of linearly independent paths through a program's source code.`,
+            label: `Cyclomatic complexity is a software metric used to indicate the complexity of a program. It is a quantitative measure of the number of linearly independent paths through a program's source code.`,
             path: "https://en.wikipedia.org/wiki/Cyclomatic_complexity",
           },
         ],
