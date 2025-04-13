a2715963c468616372b0a00dc1540fe4154b7392
Jun Jose <junners@users.noreply.github.com>
2025-04-12 23:39:32 -0700

    feat: recusrive after update rule initial draft (#200)

---
+++ src/main/rules/RecursiveAfterUpdate.ts
@@ -0,0 +1,96 @@
+import * as core from "../internals/internals";
+import { RuleCommon } from "../models/RuleCommon";
+
+export class RecursiveAfterUpdate extends RuleCommon implements core.IRuleDefinition {
+  protected qualifiedRecordTriggerTypes: Set<string> = new Set<string>([
+    "Create",
+    "Update",
+    "CreateAndUpdate",
+  ]);
+
+  constructor() {
+    super(
+      {
+        name: "RecursiveAfterUpdate",
+        label: "Recursive After Update",
+        description:
+          "After updates are meant to be used for record modifications that are not the same record that triggered the flow. Using after updates on the same record can lead to recursion and unexpected behavior. Consider using before save flows for same record updates.",
+        supportedTypes: [...core.FlowType.backEndTypes],
+        docRefs: [
+          {
+            label: "Learn about same record field updates",
+            path: "https://architect.salesforce.com/decision-guides/trigger-automation#Same_Record_Field_Updates",
+          },
+        ],
+        isConfigurable: false,
+        autoFixable: false,
+      },
+      { severity: "warning" }
+    );
+  }
+
+  public execute(flow: core.Flow): core.RuleResult {
+    const results: core.ResultDetails[] = [];
+
+    const isAfterSave = flow.start?.triggerType === "RecordAfterSave";
+    const isQualifiedTriggerTypes = this.qualifiedRecordTriggerTypes.has(
+      flow.start?.recordTriggerType
+    );
+
+    if (!isAfterSave || !isQualifiedTriggerTypes) {
+      return new core.RuleResult(this, results);
+    }
+
+    const potentialElements = flow.elements?.filter(
+      (node) => node.subtype === "recordUpdates"
+    ) as core.FlowNode[];
+
+    if (potentialElements == null || typeof potentialElements[Symbol.iterator] !== "function") {
+      return new core.RuleResult(this, results);
+    }
+
+    for (const node of potentialElements) {
+      if (
+        typeof node.element === "object" &&
+        "inputReference" in node.element &&
+        node.element.inputReference === "$Record"
+      ) {
+        results.push(new core.ResultDetails(node));
+      }
+    }
+
+    // do another search for record updates from lookups with the same object type
+    // and check whether there is references on record updates
+
+    const lookupElementsWithTheSameObjectType = flow.elements
+      ?.filter(
+        (node) =>
+          node.subtype === "recordLookups" &&
+          typeof node.element === "object" &&
+          "object" in node.element &&
+          flow.start.object === node.element["object"]
+      )
+      ?.map((node) => {
+        return node.name;
+      });
+
+    if (
+      lookupElementsWithTheSameObjectType == null ||
+      typeof lookupElementsWithTheSameObjectType[Symbol.iterator] !== "function"
+    ) {
+      return new core.RuleResult(this, results);
+    }
+
+    for (const node of potentialElements) {
+      if (
+        typeof node.element === "object" &&
+        "inputReference" in node.element &&
+        lookupElementsWithTheSameObjectType.includes(node.element.inputReference as string)
+      ) {
+        results.push(new core.ResultDetails(node));
+      }
+    }
+
+    return new core.RuleResult(this, results);
+  }
+}
