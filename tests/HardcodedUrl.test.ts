4aacbf9d8e844d4ae1e2492d7088960d206bcce0
junners <jose.junryan@gmail.com>
2025-05-04 12:26:02 -0700

    test: add missing unit tests hardcoded url rule

--- tests/HardcodedUrl.test.ts
+++ tests/HardcodedUrl.test.ts
@@ -66,7 +66,177 @@ describe("HardcodedUrl", () => {
       expect(result).toBeDefined();
       expect(result.occurs).toBe(false);
     });
+
+    it("should return empty results when url is generic", () => {
+      const flow = {
+        type: "AutoLaunchedFlow",
+        elements: [
+          {
+            name: "genericUrl",
+            subtype: "formulas",
+            metaType: "variable",
+            element: {
+              name: "genericUrl",
+              dataType: "String",
+              expression: "&quot;https://www.google.com/&quot;&amp;{!$User.Id}",
+            },
+          },
+        ],
+      } as Partial<Flow> as Flow;
+
+      const result = rule.execute(flow);
+      expect(result).toBeDefined();
+      expect(result.occurs).toBe(false);
+    });
+
+    it("should return empty results when url is like jira", () => {
+      const flow = {
+        type: "AutoLaunchedFlow",
+        elements: [
+          {
+            name: "jiraUrl",
+            subtype: "formulas",
+            metaType: "variable",
+            element: {
+              name: "jiraUrl",
+              dataType: "String",
+              expression: "&quot;https://mydomain.atlassian.net/browse/TEST-123",
+            },
+          },
+        ],
+      } as Partial<Flow> as Flow;
+
+      const result = rule.execute(flow);
+      expect(result).toBeDefined();
+      expect(result.occurs).toBe(false);
+    });
+
+    it("should return empty results when url is like confluence", () => {
+      const flow = {
+        type: "AutoLaunchedFlow",
+        elements: [
+          {
+            name: "confluenceUrl",
+            subtype: "formulas",
+            metaType: "variable",
+            element: {
+              name: "confluenceUrl",
+              dataType: "String",
+              expression:
+                "&quot;https://mydomain.atlassian.net/wiki/spaces/TEST/pages/123456789/Test+Page&quot;",
+            },
+          },
+        ],
+      } as Partial<Flow> as Flow;
+
+      const result = rule.execute(flow);
+      expect(result).toBeDefined();
+      expect(result.occurs).toBe(false);
+    });
   });
 
-  describe("error unit", () => {});
+  describe("error unit", () => {
+    describe("flow formula", () => {
+      it("should return results when hardcoding sandbox org url", () => {
+        const flow = {
+          type: "AutoLaunchedFlow",
+          elements: [
+            {
+              name: "hardcodedUrl",
+              subtype: "formulas",
+              metaType: "variable",
+              element: {
+                name: "hardcodedUrl",
+                dataType: "String",
+                expression:
+                  "&quot;https://mydomain.sandbox.my.salesforce.com/&quot;&amp;{!$User.Id}",
+              },
+            },
+          ],
+        } as Partial<Flow> as Flow;
+
+        const result = rule.execute(flow);
+        expect(result).toBeDefined();
+        expect(result.occurs).toBe(true);
+      });
+
+      it("should return results when hardcoding prod org url", () => {
+        const flow = {
+          type: "AutoLaunchedFlow",
+          elements: [
+            {
+              name: "hardcodedUrl",
+              subtype: "formulas",
+              metaType: "variable",
+              element: {
+                name: "hardcodedUrl",
+                dataType: "String",
+                expression: "&quot;https://mydomain.my.salesforce.com/&quot;&amp;{!$User.Id}",
+              },
+            },
+          ],
+        } as Partial<Flow> as Flow;
+
+        const result = rule.execute(flow);
+        expect(result).toBeDefined();
+        expect(result.occurs).toBe(true);
+      });
+    });
+
+    describe("flow variable", () => {
+      it("should return results when hardcoding sandbox org url", () => {
+        const flow = {
+          type: "AutoLaunchedFlow",
+          elements: [
+            {
+              name: "hardcodedFlowVariable",
+              subtype: "variables",
+              metaType: "variable",
+              element: {
+                name: "hardcodedFlowVariable",
+                dataType: "String",
+                isCollection: false,
+                isInput: false,
+                isOutput: false,
+                value: {
+                  stringValue: "https://mydomain.sandbox.my.salesforce.com/",
+                },
+              },
+            },
+          ],
+        } as Partial<Flow> as Flow;
+
+        const result = rule.execute(flow);
+        expect(result).toBeDefined();
+        expect(result.occurs).toBe(true);
+      });
+
+      it("should return results when hardcoding prod org url", () => {
+        const flow = {
+          type: "AutoLaunchedFlow",
+          elements: [
+            {
+              name: "hardcodedFlowVariable",
+              subtype: "variables",
+              metaType: "variable",
+              element: {
+                name: "hardcodedFlowVariable",
+                dataType: "String",
+                isCollection: false,
+                isInput: false,
+                isOutput: false,
+                value: {
+                  stringValue: "https://mydomain.my.salesforce.com/",
+                },
+              },
+            },
+          ],
+        } as Partial<Flow> as Flow;
+
+        const result = rule.execute(flow);
+        expect(result).toBeDefined();
+        expect(result.occurs).toBe(true);
+      });
+    });
+  });
 });
