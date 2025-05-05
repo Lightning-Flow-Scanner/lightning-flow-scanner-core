1de0ceeb3f07a7c96f330b688ee13dce08fc1aac
junners <jose.junryan@gmail.com>
2025-05-04 20:04:19 -0700

    test: add exported rule definitions in test

--- tests/libs/GetRuleDefinitions.test.ts
+++ tests/libs/GetRuleDefinitions.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "@jest/globals";
 import { getBetaRules } from "../../src/main/libs/GetRuleDefinitions";
+import { getBetaRules as getBetaRulesExported } from "../../src/index";
 import { BetaRuleStore } from "../../src/main/store/DefaultRuleStore";
 
 describe("GetRuleDefinitions", () => {
@@ -14,6 +15,24 @@ describe("GetRuleDefinitions", () => {
     });
 
     test.each(getBetaRules())(
+      "should match a beta rule name $name severity $severity",
+      ({ name }) => {
+        expect(BetaRuleStore).toHaveProperty(name);
+      }
+    );
+  });
+
+  describe("getBetaRules exported", () => {
+    it("should be defined", () => {
+      expect(getBetaRulesExported).toBeDefined();
+    });
+
+    it("should return an array of rule definitions", () => {
+      const rules = getBetaRulesExported();
+      expect(rules).toBeInstanceOf(Array);
+    });
+
+    test.each(getBetaRulesExported())(
       "should match a beta rule name $name severity $severity",
       ({ name }) => {
         expect(BetaRuleStore).toHaveProperty(name);
