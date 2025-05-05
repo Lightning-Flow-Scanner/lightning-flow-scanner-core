79e8b529d3f29a6df56e2f0a2dcd7ea5eef07060
junners <jose.junryan@gmail.com>
2025-05-04 21:07:29 -0700

    chore: use beta rule store on dynamic rule instantiation

--- src/main/libs/DynamicRule.ts
+++ src/main/libs/DynamicRule.ts
@@ -1,24 +1,10 @@
 import { IRuleDefinition } from "../interfaces/IRuleDefinition";
-import { DefaultRuleStore } from "../store/DefaultRuleStore";
-import { RuleLoader } from "./RuleLoader";
-import * as p from "path";
+import { BetaRuleStore, DefaultRuleStore } from "../store/DefaultRuleStore";
 
-function internalOptedInRule(ruleName: string): IRuleDefinition | never {
-  const optedInRule = RuleLoader.loadCustomRule(
-    ruleName,
-    p.join(__dirname, `../rules/${ruleName}`)
-  );
-
-  if (optedInRule) {
-    return optedInRule;
-  }
-
-  throw new Error(`Rule '${ruleName}' does not exist in the store.`);
-}
 export class DynamicRule {
   constructor(className: string) {
-    if (DefaultRuleStore[className] === undefined || DefaultRuleStore[className] === null) {
-      return internalOptedInRule(className) as IRuleDefinition;
+    if (!DefaultRuleStore.hasOwnProperty(className) && BetaRuleStore.hasOwnProperty(className)) {
+      return new BetaRuleStore[className]() as IRuleDefinition;
     }
     return new DefaultRuleStore[className]() as IRuleDefinition;
   }
