b5b7818d9e20436dad3ce206b8a503f15a336d9c
junners <jose.junryan@gmail.com>
2025-06-08 01:11:56 -0400

    feat(#230): introduce new concept for advanced suppression

---
+++ src/main/interfaces/AdvancedRuleDefintion.ts
@@ -0,0 +1,7 @@
+import type { AdvancedRuleConfig } from "./AdvancedRuleConfig";
+
+import { Flow, RuleResult } from "../internals/internals";
+
+export interface AdvancedRuleDefinition {
+  execute(flow: Flow, ruleConfiguration?: AdvancedRuleConfig): RuleResult;
+}
---
+++ src/main/models/AdvancedRule.ts
@@ -0,0 +1,44 @@
+import { AdvancedRuleConfig } from "../interfaces/AdvancedRuleConfig";
+import { AdvancedRuleDefinition } from "../interfaces/AdvancedRuleDefintion";
+import { AdvancedSuppression } from "../interfaces/AdvancedSuppression";
+import { IRuleDefinition } from "../internals/internals";
+import { Flow } from "./Flow";
+import { RuleCommon } from "./RuleCommon";
+import { RuleInfo } from "./RuleInfo";
+import { RuleResult } from "./RuleResult";
+
+export abstract class AdvancedRule extends RuleCommon {
+  constructor(
+    info: RuleInfo,
+    optional?: {
+      severity?: string;
+    }
+  ) {
+    super(info, optional);
+  }
+
+  public executeRule(flow: Flow, ruleConfiguration?: AdvancedRuleConfig): RuleResult {
+    if (!hasAdvancedRuleDefinition(this)) {
+      return new RuleResult(this as unknown as IRuleDefinition, []);
+    }
+
+    let ruleResult = this.execute(flow, ruleConfiguration);
+
+    if (hasAdvancedSuppression(this)) {
+      ruleResult = this.suppress(ruleResult, ruleConfiguration);
+    }
+
+    return ruleResult;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+const isFunction = (val: unknown): val is Function => typeof val === "function";
+
+function hasAdvancedRuleDefinition(instance: unknown): instance is AdvancedRuleDefinition {
+  return isFunction((instance as AdvancedRuleDefinition).execute);
+}
+
+function hasAdvancedSuppression(instance: unknown): instance is AdvancedSuppression {
+  return isFunction((instance as AdvancedSuppression).suppress);
+}
