7b07ea41d836c76fac4d8debd2f0841a00cb6aea
junners <jose.junryan@gmail.com>
2025-06-11 16:14:04 -0700

    chore: use IruleDefinition casting for legacy execute support

--- src/main/models/AdvancedRule.ts
+++ src/main/models/AdvancedRule.ts
@@ -26,7 +26,7 @@ export abstract class AdvancedRule extends RuleCommon implements AdvancedRuleDef
       return new RuleResult(this as unknown as IRuleDefinition, []);
     }
 
-    let ruleResult = (this as AdvancedRuleDefinition).execute(flow, ruleConfiguration);
+    let ruleResult = (this as IRuleDefinition).execute(flow, ruleConfiguration);
 
     if (hasAdvancedSuppression(this)) {
       ruleResult = this.suppress(ruleResult, ruleConfiguration);
