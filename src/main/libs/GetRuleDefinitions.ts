7708497c4cc21692d24a8d8041885b0292a4552c
junners <jose.junryan@gmail.com>
2025-08-02 10:56:08 -0700

    chore: drop support for IRuleDefinition

--- src/main/libs/GetRuleDefinitions.ts
+++ src/main/libs/GetRuleDefinitions.ts
@@ -1,10 +1,15 @@
-import { IRuleDefinition } from "../interfaces/IRuleDefinition";
+import { AdvancedRule } from "../models/AdvancedRule";
 import { BetaRuleStore, DefaultRuleStore } from "../store/DefaultRuleStore";
 import { DynamicRule } from "./DynamicRule";
 import { RuleLoader } from "./RuleLoader";
 
-export function GetRuleDefinitions(ruleConfig?: Map<string, unknown>): IRuleDefinition[] {
-  const selectedRules: IRuleDefinition[] = [];
+export function getBetaRules(): AdvancedRule[] {
+  return getBetaDefinition();
+}
+
+// eslint-disable-next-line sonarjs/cognitive-complexity
+export function GetRuleDefinitions(ruleConfig?: Map<string, unknown>): AdvancedRule[] {
+  const selectedRules: AdvancedRule[] = [];
   if (ruleConfig && ruleConfig instanceof Map) {
     for (const ruleName of ruleConfig.keys()) {
       let severity = "error";
@@ -20,13 +25,13 @@ export function GetRuleDefinitions(ruleConfig?: Map<string, unknown>): IRuleDefi
           severity = configuredSeverity;
         }
         if (configuredPath) {
-          const customRule = RuleLoader.loadCustomRule(ruleName, configuredPath) as IRuleDefinition;
+          const customRule = RuleLoader.loadCustomRule(ruleName, configuredPath) as AdvancedRule;
           if (configuredSeverity) {
             customRule.severity = severity;
           }
           selectedRules.push(customRule);
         } else {
-          const matchedRule = new DynamicRule(ruleName) as IRuleDefinition;
+          const matchedRule = new DynamicRule(ruleName) as AdvancedRule;
           if (configuredSeverity) {
             matchedRule.severity = severity;
           }
@@ -38,7 +43,7 @@ export function GetRuleDefinitions(ruleConfig?: Map<string, unknown>): IRuleDefi
     }
   } else {
     for (const rule in DefaultRuleStore) {
-      const matchedRule = new DynamicRule(rule) as IRuleDefinition;
+      const matchedRule = new DynamicRule(rule) as AdvancedRule;
       selectedRules.push(matchedRule);
     }
   }
@@ -46,7 +51,7 @@ export function GetRuleDefinitions(ruleConfig?: Map<string, unknown>): IRuleDefi
   return selectedRules;
 }
 
-export function getRules(ruleNames?: string[]): IRuleDefinition[] {
+export function getRules(ruleNames?: string[]): AdvancedRule[] {
   if (ruleNames && ruleNames.length > 0) {
     const ruleSeverityMap = new Map<string, string>(ruleNames.map((name) => [name, "error"]));
     return GetRuleDefinitions(ruleSeverityMap);
@@ -55,10 +60,6 @@ export function getRules(ruleNames?: string[]): IRuleDefinition[] {
   }
 }
 
-export function getBetaRules(): IRuleDefinition[] {
-  return getBetaDefinition();
-}
-
-function getBetaDefinition(): IRuleDefinition[] {
-  return Object.values(BetaRuleStore).map((rule) => new rule() as IRuleDefinition);
+function getBetaDefinition(): AdvancedRule[] {
+  return Object.values(BetaRuleStore).map((rule) => new rule() as AdvancedRule);
 }
